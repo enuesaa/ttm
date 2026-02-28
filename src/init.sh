0ab6b241673c19e4dfa1f7852fab40820a49996b
nua <enuesaa@gmail.com>
2026-02-28 18:39:14 +0900

    feat: try change prompt on fish

--- src/init.sh
+++ src/init.sh
@@ -1,3 +1,14 @@
+# zsh
 if [[ "$TTM" == "true" ]]; then
   echo 'PS1="%F{yellow}[ttm]%f $PS1"'
 fi
+
+# fish
+# function fish_prompt
+#   if test "$TTM" = "true"
+#     set_color yellow
+#     echo -n "[ttm] "
+#     set_color normal
+#   end
+#   echo -n (prompt_pwd) "> "
+# end
