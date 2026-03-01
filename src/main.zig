84634648e2d80d6e4cd38aa0d03708a3d492a9fe
nua <enuesaa@gmail.com>
2026-03-01 14:37:11 +0900

    refactor: remove comment-out code

--- src/main.zig
+++ src/main.zig
@@ -17,13 +17,6 @@ pub fn main() !void {
         try std.fs.File.stdout().writeAll(initsh);
         return;
     }
-    try launchCLI();
-}
-
-pub fn launchCLI() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
 
     // cli
     var runner = try cli.AppRunner.init(allocator);
@@ -48,95 +41,6 @@ pub fn launchCLI() !void {
                     },
                     .exec = ttm.cd,
                 },
-                // .subcommands = try runner.allocCommands(&.{
-                //     cli.Command{
-                //         .name = "ls",
-                //         .description = cli.Description{
-                //             .one_line = "list tmp dirs",
-                //         },
-                //         .target = cli.CommandTarget{
-                //             .action = cli.CommandAction{
-                //                 .exec = ttm.list,
-                //             },
-                //         },
-                //     },
-                //     cli.Command{
-                //         .name = "pin",
-                //         .description = cli.Description{
-                //             .one_line = "rename and keep tmp dir",
-                //         },
-                //         .target = cli.CommandTarget{
-                //             .action = cli.CommandAction{
-                //                 .positional_args = cli.PositionalArgs{
-                //                     .required = try runner.allocPositionalArgs(&.{
-                //                         .{
-                //                             .name = "FROM",
-                //                             .help = "tmpdir name",
-                //                             .value_ref = runner.mkRef(&ttm.cliargs.pinFrom),
-                //                         },
-                //                         .{
-                //                             .name = "TO",
-                //                             .help = "tmpdir name",
-                //                             .value_ref = runner.mkRef(&ttm.cliargs.pinTo),
-                //                         },
-                //                     }),
-                //                 },
-                //                 .exec = ttm.pin,
-                //             },
-                //         },
-                //     },
-                //     cli.Command{
-                //         .name = "rm",
-                //         .description = cli.Description{
-                //             .one_line = "remove tmp dir",
-                //         },
-                //         .target = cli.CommandTarget{
-                //             .action = cli.CommandAction{
-                //                 .positional_args = cli.PositionalArgs{
-                //                     .required = try runner.allocPositionalArgs(&.{
-                //                         .{
-                //                             .name = "REMOVE",
-                //                             .help = "tmpdir name",
-                //                             .value_ref = runner.mkRef(&ttm.cliargs.removeDir),
-                //                         },
-                //                     }),
-                //                 },
-                //                 .exec = ttm.remove,
-                //             },
-                //         },
-                //     },
-                //     cli.Command{
-                //         .name = "prune",
-                //         .description = cli.Description{
-                //             .one_line = "remove archived tmp dirs",
-                //         },
-                //         .target = cli.CommandTarget{
-                //             .action = cli.CommandAction{
-                //                 .exec = ttm.prune,
-                //             },
-                //         },
-                //     },
-                //     cli.Command{
-                //         .name = "cd",
-                //         .description = cli.Description{
-                //             .one_line = "cd",
-                //         },
-                //         .target = cli.CommandTarget{
-                //             .action = cli.CommandAction{
-                //                 .positional_args = cli.PositionalArgs{
-                //                     .optional = try runner.allocPositionalArgs(&.{
-                //                         .{
-                //                             .name = "to",
-                //                             .help = "to dir name",
-                //                             .value_ref = runner.mkRef(&ttm.cliargs.cdTo),
-                //                         },
-                //                     }),
-                //                 },
-                //                 .exec = ttm.cd,
-                //             },
-                //         },
-                //     },
-                // }),
             },
         },
         .help_config = cli.HelpConfig{
