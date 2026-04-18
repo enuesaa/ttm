acdbe6bffde37b229e57d3b100898a4178fc53b1
nua <enuesaa@gmail.com>
2026-04-18 18:06:30 +0900

    feat: use new stdin reader

--- src/pkg/prompt.zig
+++ src/pkg/prompt.zig
@@ -3,27 +3,22 @@ const std = @import("std");
 pub var io: ?std.Io = null;
 
 pub fn ask(allocator: std.mem.Allocator, text: []const u8, defaultValue: []const u8) ![]u8 {
-    const stdin = std.Io.File.stdin();
     if (std.mem.eql(u8, defaultValue, "")) {
         std.debug.print("{s}? {s}: {s}", .{ "\x1b[33m", text, "\x1b[0m" });
     } else {
         std.debug.print("{s}? {s} (default {s}): {s}", .{ "\x1b[33m", text, defaultValue, "\x1b[0m" });
     }
 
-    var buf: []const u8 = undefined;
-    var idx: usize = 0;
+    var buf: [100]u8 = undefined;
+    const stdin = std.Io.File.stdin();
+    var reader = stdin.reader(io.?, &buf);
 
-    while (idx < buf.len) {
-        const b: []const []u8 = undefined;
-        const n = try stdin.readStreaming(io.?, b);
-        if (n == 0) {
-            break;
-        }
-        // buf[idx] = b[]; // TODO
-        idx += 1;
-    }
-    if (idx == 0) {
+    const line = reader.interface.takeDelimiterExclusive('\n') catch |err| switch (err) {
+        error.EndOfStream => return try allocator.dupe(u8, defaultValue),
+        else => return err,
+    };
+    if (line.len == 0) {
         return try allocator.dupe(u8, defaultValue);
     }
-    return try allocator.dupe(u8, buf[0..idx]);
+    return try allocator.dupe(u8, line);
 }
